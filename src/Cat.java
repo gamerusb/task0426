0412af333fd8253d5add50f618ea51af7401afb6
gamerusb <kratrol@gmail.com>
2022-10-13 19:39:54 +0200

    Массивы строк

--- src/Cat.java
+++ src/Cat.java
@@ -6,7 +6,28 @@ public class Cat {
     public static void main(String[] args) throws IOException {
         int[] numbers = new int[5];
         numbers [0] = 10;
-        System.out.println(numbers[0]);
+        String[] strings = new String[3];
+        strings [0] = "Hello";
+        strings [1] = "Bue";
+        strings [2] = "Java";
+
+        for (String string:strings){
+            System.out.println(string);
+        }
+        int[] numbers1 = {1,2,3};
+        int sum = 0;
+        for(int x: numbers1){
+            sum = sum+x;
+            System.out.println();
+            System.out.println(sum);
+        }
+
+
+        int value = 0;
+        String s = "nulls";
+        System.out.println(s);
 
     }
 }
+
+//Сделать пару примеров по массивам, не забыть
