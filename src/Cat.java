e5b79fc0743d4111c25423e0469e875892b99e19
gamerusb <kratrol@gmail.com>
2022-10-13 18:55:00 +0200

    Массивы строк

---
+++ src/Cat.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cat {
+    public static void main(String[] args) throws IOException {
+        int[] numbers = new int[5];
+        numbers [0] = 10;
+        System.out.println(numbers[0]);
+
+    }
+}
